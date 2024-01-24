045a1f19e928173c8d20c888b21ff29b576d2453
auredeni31 <aurel.denizet@gmail.com>
2024-01-24 14:59:27 +0100

    Initial commit - TP4

---
+++ src/TP2D/MainInterface.java
@@ -0,0 +1,84 @@
+package TP2D;
+
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class MainInterface extends JFrame implements KeyListener {
+    TileManager tileManager = new TileManager(48,48,"./img/tileSet.png");
+    Dungeon dungeon = new Dungeon("./gameplay/level1.txt",tileManager);
+    Hero hero = Hero.getInstance();
+    GameRender panel = new GameRender(dungeon,hero);
+    public MainInterface() throws HeadlessException {
+        super();
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        this.setSize(new Dimension(dungeon.getWidth()* tileManager.getWidth(), dungeon.getHeight()*tileManager.getHeigth()));
+        this.addKeyListener(this);
+
+        ActionListener animationTimer=new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repaint();
+                final int speed=10;
+                if (hero.isWalking()){
+                    switch (hero.getOrientation()){
+                        case LEFT:  hero.moveIfPossible(-speed,0,dungeon);
+                            break;
+                        case RIGHT: hero.moveIfPossible(speed,0,dungeon);
+                            break;
+                        case UP:    hero.moveIfPossible(0,-speed,dungeon);
+                            break;
+                        case DOWN:  hero.moveIfPossible(0,speed,dungeon);
+                            break;
+
+                    }
+                }
+            }
+        };
+        Timer timer = new Timer(50,animationTimer);
+        timer.start();
+    }
+
+    public static void main(String[] args){
+        MainInterface mainInterface = new MainInterface();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_LEFT:
+                hero.setOrientation(Orientation.LEFT);
+                hero.setWalking(true);
+                break;
+            case KeyEvent.VK_RIGHT:
+                hero.setOrientation(Orientation.RIGHT);
+                hero.setWalking(true);
+                break;
+            case KeyEvent.VK_UP:
+                hero.setOrientation(Orientation.UP);
+                hero.setWalking(true);
+                break;
+            case KeyEvent.VK_DOWN:
+                hero.setOrientation(Orientation.DOWN);
+                hero.setWalking(true);
+                break;
+        }
+        this.repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        hero.setWalking(false);
+    }
+}
